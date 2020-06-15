4a86af41d55d95b6ce9bcf7ae0d345ad94f7486a
ajg4 <alexander.j.goetz@gmail.com>
2020-06-16 00:15:55 +0200

    merging helper

--- thesis/12.py
+++ thesis/12.py
@@ -1,9 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-sys.path.append('//cern.ch/dfs/Users/a/agoetz/Desktop/corona_work_place/helper')
-from exp_speckles import *
-from the_speckles import the_speckles,radial_profile
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+from multiprocessing.pool import ThreadPool
+import gc
+
+
+def furierer(files,av,pxx,pxy,coreid):
+    imft=np.zeros((len(files),pxy,pxx))
+    for i in range(len(files)):
+        cim=files[i]-av
+        imft[i]=np.abs(np.fft.fftshift(np.fft.fft2(np.fft.fftshift(cim))))**2
+        if(coreid==0):
+            print("fft ",i,len(files))
+    return(imft)
+        
+
+def exp_speckles(path, pxx,pxy,cores):
+    waterfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    waterfiles=waterfiles[:-1]
+    
+    img=np.zeros((len(waterfiles),pxy,pxx))
+    for i in range(len(waterfiles)):
+        if(waterfiles[i][-3:]=="tif"):
+            im = np.array(Image.open(path+waterfiles[i]))
+            im=im[:pxx,150:pxx+150]
+            img[i]=im
+            print("loaded file ",i,len(waterfiles))
+    
+    cut=len(waterfiles)/cores
+    cut2=len(waterfiles)-int((cut%1)*4)
+    waterfiles=waterfiles[:cut2]
+    
+    wavim=np.sum(img,axis=0)/len(waterfiles)
+    
+    imft=np.zeros((len(waterfiles),pxy,pxx))
+    
+    pool=ThreadPool(processes=cores)
+    sl=int(len(waterfiles)/cores)
+    proc=[]
+    for i in range(cores):
+        proc.append(pool.apply_async(furierer,(img[i*sl:(i+1)*sl],wavim,pxy,pxx,i)))
+    for i in range(cores):
+        imft[i*sl:(i+1)*sl]=proc[i].get()
+    
+    wavimft=np.sum(imft,axis=0)/len(waterfiles)
+    del(imft)
+    gc.collect()
+    
+    
+    return(wavim,wavimft)
 
 
 #Beam
